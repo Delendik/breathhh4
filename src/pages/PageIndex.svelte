363108762a41a28e49c1da8d61f66d8823e86b75
amasalov <delsol@bk.ru>
2021-05-15 11:32:59 +0300

    login

---
+++ src/pages/PageIndex.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import LayoutCenter from "../comp/LayoutCenter.svelte";
+</script>
+
+<LayoutCenter>
+  <div>
+    <h1 class="title">Breathhh</h1>
+    <a class="link" href="#">Install from Google Store</a>
+  </div>
+</LayoutCenter>
+
+<style>
+  .title {
+    margin: 0 0 25px;
+    text-align: center;
+  }
+
+  .link {
+    display: inline-flex;
+    padding: 12px 24px;
+    background-color: #eb6135;
+    font-size: 18px;
+    color: white;
+    box-shadow: 0px 2px 0px #d33f32;
+    border-radius: 4px;
+  }
+
+  :global(body) {
+    padding: 0;
+  }
+</style>
---
+++ src/pages/PageLogin.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import LayoutCenter from "../comp/LayoutCenter.svelte";
+</script>
+
+<LayoutCenter>
+  <div>
+    <h1 class="title">Please login to start Breathhh</h1>
+    <p class="subtitle">
+      This is to fine-tune the beneficial practices specifically for you
+    </p>
+    <div class="btn-warp">
+      <a class="link" href="#"><span>Continue with Google</span></a>
+      <a class="link" href="#"><span>Continue with Facebook</span></a>
+    </div>
+  </div>
+</LayoutCenter>
+
+<style>
+  .title {
+    font-size: 32px;
+    color: #3d3a4b;
+    text-align: center;
+    margin: 0 0 15px;
+  }
+
+  .subtitle {
+    font-size: 20px;
+    color: #615f68;
+    text-align: center;
+  }
+
+  .btn-warp {
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    gap: 12px;
+    margin-top: 45px;
+  }
+
+  .link {
+    position: relative;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.16);
+    border-radius: 4px;
+    font-size: 18px;
+    color: #3d3a4b;
+    padding: 15px 15px 15px 64px;
+  }
+
+  .link::before {
+    content: "";
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    width: 24px;
+    height: 24px;
+    background-color: red;
+  }
+</style>
