47737b81c0cebe4e1b337f1feb6069376f92b0c8
amasalov <delsol@bk.ru>
2021-05-15 14:15:34 +0300

    global styles

--- src/App.svelte
+++ src/App.svelte
@@ -7,23 +7,8 @@
   export let url = "";
 </script>
 
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <Router {url}>
   <Route path="/login" component={PageLogin} />
   <Route path="/onboarding" component={PageOnBoarding} />
   <Route path="/" component={PageIndex} />
 </Router>
-
-<style>
-  :global(body) {
-    padding: 0;
-    font-family: "DM Sans", sans-serif;
-  }
-</style>
