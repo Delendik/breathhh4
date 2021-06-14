96398103ab8e9b8e6ac84bcd0fb96f2065a120c2
itslooklike <delsol@bk.ru>
2021-06-14 10:32:44 +0300

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
 import './index.css'
 import { App } from './App'
+
+Sentry.init({
+  dsn: 'https://421dffc7895f4d5e9b8f9c1c22532dde@o114540.ingest.sentry.io/5814909',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
