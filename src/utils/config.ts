ff80d2cf5d78b972ef0177f7a5fbe4fa4dddf1c4
itslooklike <delsol@bk.ru>
2021-07-21 16:14:53 +0300

    update id

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,12 +3,19 @@ export const apiUrl = 'https://api.breathhh.app'
 export const apiUrlFacebook = apiUrl + '/auth/facebook'
 export const apiUrlGoogle = apiUrl + '/auth/google_oauth2'
 export const chromeExtUrl = 'https://google.com'
-const chromeExtId = 'bmllbkjcollkklanejokmbnmgmknkkph'
 
+const chromeExtId_OLD = 'bmllbkjcollkklanejokmbnmgmknkkph'
+const chromeExtId = 'cllimaakonijffcoofcdjmgdibcdiabd'
+
+// FIXME: нужно будет удалить `chromeExtId_OLD`, когда все будут сидеть на новой версии
 export const sendMessageToExt = (type: string) => {
   try {
     chrome.runtime.sendMessage(chromeExtId, { type })
   } catch (error) {
     console.log('Extension not installed')
   }
+
+  try {
+    chrome.runtime.sendMessage(chromeExtId_OLD, { type })
+  } catch {}
 }
