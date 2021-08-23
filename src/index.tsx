4f603ab82a0b097117027668c7b09a700d2c0ebf
itslooklike <delsol@bk.ru>
2021-08-23 20:41:11 +0300

    add rollbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
+import Rollbar from 'rollbar'
+// @ts-ignore
+import { Provider, ErrorBoundary } from '@rollbar/react'
 
 import './index.css'
 import { App } from './App'
 
-if (process.env.NODE_ENV === `production`) {
-  Sentry.init({
-    dsn: `https://421dffc7895f4d5e9b8f9c1c22532dde@o114540.ingest.sentry.io/5814909`,
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-  })
+const rollbarConfig = {
+  accessToken: `960c63c90161452fa830caa464230012`,
+  environment: process.env.NODE_ENV === `production` ? `production` : `development`,
 }
+
+const rollbar = new Rollbar(rollbarConfig)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider instance={rollbar}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>,
   document.getElementById(`root`)
 )
